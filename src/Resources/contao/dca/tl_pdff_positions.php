37ab6801727a197e317a75fcb1081fec312026e7
Hagen Klemp <info@softleister.de>
2023-12-12 23:12:47 +0100

    Fehler deleteConfirm gefixt

--- src/Resources/contao/dca/tl_pdff_positions.php
+++ src/Resources/contao/dca/tl_pdff_positions.php
@@ -88,7 +88,7 @@ $GLOBALS['TL_DCA']['tl_pdff_positions'] = array
                 'label'               => &$GLOBALS['TL_LANG']['tl_pdff_positions']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.gif',
-                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
             ),
             'toggle' => array
             (
